8131b9b2663d160ecde0b9cbd94123e3d54c6919
Matt Lurz <matt@definite-software.com>
2017-06-11 10:21:06 -0700

    initial loopback-ssl setup

--- server/server.js
+++ server/server.js
@@ -2,20 +2,12 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var loopbackSSL = require('loopback-ssl');
 
 var app = module.exports = loopback();
 
 app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
+  return loopbackSSL.startServer(app);
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
